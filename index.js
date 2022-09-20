301af651c7460a05f63a44b7721117eedbeba413
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2022-09-20 15:57:07 +0600

    booking api available api

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -12,31 +11,73 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nexoexb.mongodb.net/health_care?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const servicesCollection = client.db('health_care').collection('services');
-        app.get('/service', async(req, res) => {
-            const query ={};
-            const cursor = servicesCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-    }
-    finally{
-        console.log(e);
-        process.exit(1);
-    }
-} 
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client.db("health_care").collection("services");
+    const bookingCollection = client.db("health_care").collection("booking");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+   
+    app.get('/available', async (req, res) => {
+      const date = req.query.date;
+
+      const services = await servicesCollection.find().toArray();
+
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      services.forEach(service => {
+        const serviceBookings = bookings.filter(book => book.treatment === service.name);
+        const bookedSlots = serviceBookings.map(book => book.slot);
+        const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+        service.slots = available;
+      });
+
+
+      res.send(services);
+    })
+
+    app.get('/booking', async (req, res) => {
+      const patient = req.query.patient;
+      const query = {patient: patient};
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
+    app.post('/booking', async (req, res) =>{
+      const booking =req.body;
+      const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+      const exists = await bookingCollection.findOne(query);
+      if(exists){
+        return res.send({success: false, booking: exists})
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({success: true, result});
+    })
+
+  } finally {
+    
+  }
+}
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send(' Hello Health Care!')
-})
+app.get("/", (req, res) => {
+  res.send(" Hello Health Care!");
+});
 
 app.listen(port, () => {
-  console.log(`Healthcare app listening on port ${port}`)
-})
+  console.log(`Healthcare app listening on port ${port}`);
+});
